826be05af6bedc105bd0ac31d0925c248aabdada
Alexandre BECAVIN <alexandre.becavin@gmail.com>
2023-08-22 16:24:04 +0200

    Installation vuetify

---
+++ src/components/Projection.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col cols="2">
+      <v-sheet class="pa-2 ma-2">
+        <p>Test</p>
+      </v-sheet>
+    </v-col>
+
+    <v-col cols="8">
+      <v-sheet class="pa-2 ma-2">
+        <p>Test</p>
+      </v-sheet>
+    </v-col>
+
+    <v-col cols="2">
+      <v-sheet class="pa-2 ma-2">
+        <p>Test</p>
+      </v-sheet>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <Projection />
+</template>
+
+<script setup>
+  import Projection from '@/components/Projection.vue'
+</script>
