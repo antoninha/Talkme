54bd6aa61e450a894a97e996aae2127b50c617fd
David-49 <david.dognin@proton.me>
2023-08-23 17:14:54 +0200

    Add charts

--- src/views/Configurator.vue
+++ src/views/Configurator.vue
@@ -8,7 +8,17 @@
 
     <v-col cols="8">
       <v-sheet class="pa-2 ma-2">
-        <p>Test</p>
+        <v-row>
+          <v-col
+            v-for="(module, index) in modules"
+            :key="index"
+            :cols="module.double ? 12 : 6"
+          >
+            <ContainerModule :title="module.label" :double="module.double">
+              <component :is="module.component" />
+            </ContainerModule>
+          </v-col>
+        </v-row>
       </v-sheet>
     </v-col>
 
@@ -20,6 +30,35 @@
   </v-row>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import ContainerModule from "@/components/modulesTool/ContainerModule.vue";
+import BarModule from "@/components/modulesTool/BarModule.vue";
+import DoughnutModule from "@/components/modulesTool/DoughnutModule.vue";
 
-<style></style>
+const modules = ref([
+  {
+    label: "Gestion des commandes et des ventes",
+    double: true,
+    component: BarModule,
+  },
+  {
+    label: "Bar chart",
+    double: false,
+    component: BarModule,
+  },
+  {
+    label: "Doughnut chart",
+    double: false,
+    component: DoughnutModule,
+  },
+]);
+</script>
+
+<style>
+.container-icon {
+  border-radius: 10px;
+  background: #ffe3ac;
+  padding: 10px;
+}
+</style>
