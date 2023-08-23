0fa38c1a8a08b3317d08e6e455837b5ce9cf87c5
Antonin <antonin.hausseguy@gmail.com>
2023-08-23 20:27:36 +0200

    left column

---
+++ src/components/DialogInfo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="text-center">
+    <v-btn
+        variant="outlined" 
+        color="#CA8702">
+    
+        En savoir plus
+
+      <v-dialog
+        v-model="dialog"
+        activator="parent"
+        width="auto"
+      >
+        <v-card>
+          <v-card-text>
+            {{item.title}}
+        </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-btn>
+  </div>
+</template>
+<script setup>
+    import { ref } from 'vue';
+
+    defineProps({
+        item:Object
+    })
+
+    const dialog = ref(false)
+  
+</script>
--- src/views/Configurator.vue
+++ src/views/Configurator.vue
@@ -1,18 +1,18 @@
 <template>
   <v-row>
-    <v-col cols="2">
+    <v-col cols="3">
+      <v-sheet class="pa-2 ma-2">
+        <Choice></Choice>
+      </v-sheet>
+    </v-col>
+
+    <v-col cols="6">
       <v-sheet class="pa-2 ma-2">
         <p>Test</p>
       </v-sheet>
     </v-col>
 
-    <v-col cols="8">
-      <v-sheet class="pa-2 ma-2">
-        <p>Test</p>
-      </v-sheet>
-    </v-col>
-
-    <v-col cols="2">
+    <v-col cols="3">
       <v-sheet class="pa-2 ma-2">
         <p>Test</p>
       </v-sheet>
@@ -20,6 +20,7 @@
   </v-row>
 </template>
 
-<script setup></script>
+<script setup>
+  import Choice from '@/components/Choice.vue'
 
-<style></style>
+</script>
