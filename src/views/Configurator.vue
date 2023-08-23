d3ba4c575916cf36d2e3ba14b135f2246fa76e0b
Alexandre BECAVIN <alexandre.becavin@gmail.com>
2023-08-23 20:11:56 +0200

    Ajout main content

--- src/views/Configurator.vue
+++ src/views/Configurator.vue
@@ -10,12 +10,12 @@
       <v-sheet class="pa-2 ma-2">
         <v-row>
           <v-col
-            v-for="(module, index) in modules"
+            v-for="(selectedModule, index) in selectedModules"
             :key="index"
-            :cols="module.double ? 12 : 6"
+            :cols="selectedModule.contentModule.double ? 12 : 6"
           >
-            <ContainerModule :title="module.label" :double="module.double">
-              <component :is="module.component" />
+            <ContainerModule :title="selectedModule.contentModule.label" :double="selectedModule.contentModule.double">
+              <component :is="selectedModule.contentModule.component" />
             </ContainerModule>
           </v-col>
         </v-row>
@@ -35,66 +35,72 @@ import { ref } from "vue";
 import ContainerModule from "@/components/modulesTool/ContainerModule.vue";
 import BarModule from "@/components/modulesTool/BarModule.vue";
 import DoughnutModule from "@/components/modulesTool/DoughnutModule.vue";
-import ResultConfiguration from '@/components/result/ResultConfiguration.vue';
-import { ref } from 'vue';
+import ResultConfiguration from '@/components/modulesResult/ResultConfiguration.vue';
 
 const updateSelectedModules = (newValue) => {
   selectedModules.value = newValue;
 }
 
-// const modules = [
+// const modules = ref([
 //   {
 //     id: 1,
 //     title: "Commandes et ventes",
 //     icon: "mdi-ticket-confirmation-outline",
-//     color: "#1234AD"
+//     color: "#1234AD",
+//     contentModule: {
+//         label: "Doughnut chart",
+//         double: false,
+//         component: DoughnutModule,
+//       },
 //   },
 //   {
 //     id: 2,
 //     title: "Facturation",
 //     icon: "mdi-file-document-outline",
-//     color: "#707070"
+//     color: "#707070",
+//     contentModule: {
+//         label: "Bar chart",
+//         double: false,
+//         component: BarModule,
+//       },
 //   },
 //   {
 //     id: 3,
 //     title: "Production",
 //     icon: "mdi-factory",
-//     color: "#B1340C"
+//     color: "#B1340C",
+//     contentModule: {
+//       label: "Gestion des commandes et des ventes",
+//       double: true,
+//       component: BarModule,
+//     }
 //   }
-// ];
-
-const modules = ref([
-  {
-    label: "Gestion des commandes et des ventes",
-    double: true,
-    component: BarModule,
-  },
-  {
-    label: "Bar chart",
-    double: false,
-    component: BarModule,
-  },
-  {
-    label: "Doughnut chart",
-    double: false,
-    component: DoughnutModule,
-  },
-]);
+// ]);
 
 const selectedModules = ref([
+{
+    id: 1,
+    title: "Commandes et ventes",
+    icon: "mdi-ticket-confirmation-outline",
+    color: "#1234AD",
+    contentModule: {
+        label: "Doughnut chart",
+        double: false,
+        component: DoughnutModule,
+      },
+  },
   {
-      id: 1,
-      title: "Commandes et ventes",
-      icon: "mdi-ticket-confirmation-outline",
-      color: "#1234AD"
-    },
-    {
-      id: 2,
-      title: "Facturation",
-      icon: "mdi-file-document-outline",
-      color: "#707070"
-    }
-  ]);
+    id: 2,
+    title: "Facturation",
+    icon: "mdi-file-document-outline",
+    color: "#707070",
+    contentModule: {
+        label: "Bar chart",
+        double: false,
+        component: BarModule,
+      },
+  },
+]);
   
 </script>
 
