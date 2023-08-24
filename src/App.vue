8ac0456063e8cc06626fe9a22b5dd0eab6582983
Alexandre BECAVIN <alexandre.becavin@gmail.com>
2023-08-24 15:21:04 +0200

    tutorials

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,11 @@
 <script setup>
   //
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+* {
+  font-family: 'Poppins', sans-serif;
+}
+</style>
--- src/components/modulesResult/ResultDrawer.vue
+++ src/components/modulesResult/ResultDrawer.vue
@@ -1,23 +1,25 @@
 <template>
   <v-col cols="3" :class="{active: props.display}" class="drawer">
-    <div class="pa-2 ma-2">
-      <div class="close">
-        <v-btn variant="text" icon="mdi-close" color="#CA8702" @click="$emit('toggle', false)"></v-btn>
+    <div class="fixedDrawer v-col-3">
+      <div class="pa-2 ma-2">
+        <div class="close">
+          <v-btn variant="text" icon="mdi-close" color="#CA8702" @click="$emit('toggle', false)"></v-btn>
+        </div>
+
+
+        <h2 class="mt-5 mb-5">Votre sélection</h2>
+        <v-divider class="mb-10"></v-divider>
+
+        <div v-for="(selectedModule, index) in selectedModules" :key="index">
+          <ItemResultModule 
+            @updateSelectedModules="updateSelectedModules"
+            @updateCurrentModuleId="updateCurrentModuleId"
+            :selectedModule="selectedModule"
+            :selectedModules="selectedModules"
+          />
+        </div>
+
       </div>
-
-
-      <h2 class="mt-5 mb-5">Votre sélection</h2>
-      <v-divider class="mb-10"></v-divider>
-
-      <div v-for="(selectedModule, index) in selectedModules" :key="index">
-        <ItemResultModule 
-          @updateSelectedModules="updateSelectedModules"
-          @updateCurrentModuleId="updateCurrentModuleId"
-          :selectedModule="selectedModule"
-          :selectedModules="selectedModules"
-        />
-      </div>
-
     </div>
   </v-col>
 </template>
@@ -54,6 +56,11 @@ const updateCurrentModuleId = (newValue) => {
   display: block;
 }
 
+.fixedDrawer {
+  position: fixed;
+  right: 0;
+  top: 0;
+}
 .close {
   text-align: right;
 }
